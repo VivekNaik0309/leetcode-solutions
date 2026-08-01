0f35ed57b482a27862b25c07a2c5e69fd7ef31d6
VivekNaik0309 <viveknaik0309>
2026-08-01 18:51:08 +0530

    largest element code

--- Easy/Largest_element_in_array/Largest.java
+++ Easy/Largest_element_in_array/Largest.java
@@ -2,5 +2,15 @@ public class Largest {
     public static void main(String args[]){
         int arr[] = {3,2,1,5,2};
         
+        //Using Brute force=>Time Complexity:n log n , Space complexity: sort first and last is largest
+
+        int largest1 = arr[0];
+
+        for(int i =0;i<arr.length-1;i++){
+            if(largest1<arr[i]){
+                largest1=arr[i];
+            }
+        }
+        System.out.println(largest1);
     }
 }
