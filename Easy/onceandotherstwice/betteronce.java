19926923a1ebf4028072efbe06cd2e135ef6a3ff
VivekNaik0309 <viveknaik0309>
2026-08-03 23:15:37 +0530

    using better approach find the number that appears once, and others twice

---
+++ Easy/onceandotherstwice/betteronce.java
@@ -0,0 +1,29 @@
+public class betteronce {
+    public static void main(String[] args) {
+
+        int arr[] = {1,1,2,3,3,4,4,5,5};
+       
+        int maxi =0;
+        //finding the max value in the array
+        for(int i=0;i<arr.length;i++){
+            maxi=Math.max(arr[i],maxi);
+        }
+
+        //always use this when doing hashing
+        int hash[]=new int[maxi+1];
+        
+        //counting the frequency
+        for(int i=0;i<arr.length;i++){
+            hash[arr[i]]=hash[arr[i]]+1;
+        }
+
+        //print the found value and use return otherwise it will show arrayofindex
+        for(int i=0;i<arr.length;i++){
+            if(hash[i]==1){
+                System.out.println(i);
+                return;
+            }
+        }
+    }
+} 
+
