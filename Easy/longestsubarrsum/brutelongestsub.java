d7f26fc498e9bdf2b0904fa16d000096b1be1a8f
VivekNaik0309 <viveknaik0309>
2026-08-05 21:44:04 +0530

    longest sum array (positive) using brute approach

---
+++ Easy/longestsubarrsum/brutelongestsub.java
@@ -0,0 +1,25 @@
+
+public class brutelongestsub {
+    public static void main(String[] args) {
+        
+        int arr[]= {1,2,3,1,1,1,4,2,3};
+        int n = arr.length;
+        int k=3;
+        
+        int length=0;
+        for(int i=0;i<n;i++){
+           
+            for(int j=i;j<n;j++){
+                int sum=0;
+                
+                for(int q=i;q<=j;q++){
+                    sum+=arr[q];
+                }
+                    if(sum==k){
+                        length=Math.max(length,j-i+1);
+                }
+            }
+        }
+        System.out.println(length);
+    }
+}
