2c34200180f99ae5798888760a32f83f67d1b682
VivekNaik0309 <viveknaik0309>
2026-08-09 22:52:19 +0530

    added optimal approach for better max sub arr

--- Medium/max subarray sum/optimalmaxsub.java
+++ Medium/max subarray sum/optimalmaxsub.java
@@ -1,4 +1,43 @@
-
 public class optimalmaxsub {
-    
+    public static void main(String[] args) {
+
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int n = arr.length;
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        int start = 0;
+        int ansStart = -1;
+        int ansEnd = -1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (sum == 0) {
+                start = i;
+            }
+
+            sum = sum + arr[i];
+
+            if (sum > max) {
+                max = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.println("Maximum Sum = " + max);
+        System.out.println("Start Index = " + ansStart);
+        System.out.println("End Index = " + ansEnd);
+
+        System.out.println("Subarray:");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
