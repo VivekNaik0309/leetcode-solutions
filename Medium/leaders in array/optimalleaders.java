89f75d3e2124ef843c85ba3b0dae7f04d8a63aaa
VivekNaik0309 <viveknaik0309>
2026-08-13 23:44:19 +0530

    added optimal approach for leader in array

--- Medium/leaders in array/optimalleaders.java
+++ Medium/leaders in array/optimalleaders.java
@@ -1,4 +1,20 @@
 import java.util.*;
 public class optimalleaders {
-    
+    public static void main(String[] args) {
+        
+        int arr[]={10,22,12,3,0,6};
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        int maxi=0;
+        for(int i=arr.length-1;i>=0;i--){
+
+            if(arr[i]>maxi){
+                maxi=arr[i];
+                ans.add(arr[i]);
+            }
+        }
+        System.out.print(ans);
+    }
 }
+//tc=O(N)
+//sc=we have used arraylist just to return so in worst case if everyone is leader than O(N)
